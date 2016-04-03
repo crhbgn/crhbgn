b740a762490eb335b3ab31a35cb9562a0d3d448a
Wasiliy Besedin <besco@yabesco.ru>
2016-04-04 01:42:48 +0300

    commit

--- install_sippy.sh
+++ install_sippy.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 
+start() {
+  echo -n "The script destroys all data on hard disk. Are you sure you want to start? (Yes/No):"
+  read yesno
+  if [ $yesno =~ ^([yY][eE][sS]|[yY])$ ]
+  then
+    echo "Installation starting"
+  else
+    echo "Instalation aborted"
+  fi
+}
+
 getIface() {
   p=1
 
@@ -19,26 +30,35 @@ getIface() {
     if [ $p -eq $numFace ]
     then
       iFace=$i
-    fi 
+    fi
     p=`expr $p + 1`
   done
 }
 
 getIp() {
   ifaceIp=`ifconfig | grep inet | grep broad | awk '{print $2}'`
+  gwIp=`route -n get default | grep gateway | awk '{print $2}'`
 }
 
 setIp() {
   getIp
   getIface
   echo -n "Enter IP addres ($ifaceIp, right? Press enter) > "
-  read ipaddr 
+  read ipaddr
   if [ -z $ipaddr ]
   then
     ipaddr=$ifaceIp
   fi
+
+  echo -n "Enter GATEWAY addres ($gwIp, right? Press enter) > "
+  read gwaddr
+  if [ -z $gwaddr ]
+  then
+    gwaddr=$gwIp
+  fi
+
   echo -n "Enter netmask (255.255.255.0, right? Press enter) > "
-  read netmask 
+  read netmask
   if [ -z $netmask ]
   then
     netmask="255.255.255.0"
@@ -47,6 +67,7 @@ setIp() {
   echo "Current settings:"
   echo "IP addr  : $ipaddr"
   echo "Netmask  : $netmask"
+  echo "Gateway  : $gwaddr"
   echo "Interface: $iFace"
   echo ""
 }
@@ -56,7 +77,7 @@ destroyDisk() {
   rc=`gpart destroy -F /dev/$disk`
   if [ $? -ne "0" ]
   then
-    #msg=`cat /tmp/inst-rc` 
+    #msg=`cat /tmp/inst-rc`
     echo "Destroy failed. Something wrong: $msg"
   fi
 }
@@ -66,25 +87,25 @@ getDisk() {
 }
 
 createParts() {
-  rc=`gpart create -s gpt /dev/$disk` 
-  if [ $? -ne "0" ] 
-  then
-    echo "Error. Something wrong:"
-  fi
-  
-  rc=`gpart add -s 64K -t freebsd-boot /dev/$disk` 
+  rc=`gpart create -s gpt /dev/$disk`
   if [ $? -ne "0" ]
   then
     echo "Error. Something wrong:"
   fi
 
-  rc=`gpart add -s 4G -t freebsd-swap -l swap0 /dev/$disk` 
+  rc=`gpart add -s 64K -t freebsd-boot /dev/$disk`
   if [ $? -ne "0" ]
   then
     echo "Error. Something wrong:"
   fi
 
-  rc=`gpart add -t freebsd-zfs -l disk0 /dev/$disk` 
+  rc=`gpart add -s 4G -t freebsd-swap -l swap0 /dev/$disk`
+  if [ $? -ne "0" ]
+  then
+    echo "Error. Something wrong:"
+  fi
+
+  rc=`gpart add -t freebsd-zfs -l disk0 /dev/$disk`
   if [ $? -ne "0" ]
   then
     echo "Error. Something wrong:"
@@ -93,30 +114,23 @@ createParts() {
   rc=`gpart bootcode -b /boot/pmbr -p /boot/gptzfsboot -i 1 /dev/$disk`
   if [ $? -ne "0" ]
   then
-    echo "Install ZFS bootcode failed. Something wrong: $msg" 
+    echo "Install ZFS bootcode failed. Something wrong: $msg"
   fi
 
   echo $rc
-} 
+}
 
 
 createZFSparts() {
   `zpool create -f zroot /dev/gpt/disk0`
   `zpool set bootfs=zroot zroot`
-   
+
    `zfs create -o mountpoint=/tmp/zroot/tmp zroot/tmp`
 }
 
 downloadImages() {
 
   #`scp besco@10.101.0.16:./mnt/zfs-images/\*.gz /tmp/zroot/tmp`
-  scp besco@yabesco.ru:/mnt/ada2/usr/home/besco/111/root.gz ./
-  scp besco@yabesco.ru:/mnt/ada2/usr/home/besco/111/storage.gz ./
-  scp besco@yabesco.ru:/mnt/ada2/usr/home/besco/111/usr-home.gz ./
-  scp besco@yabesco.ru:/mnt/ada2/usr/home/besco/111/usr.gz ./
-  scp besco@yabesco.ru:/mnt/ada2/usr/home/besco/111/var.gz ./
-  scp besco@yabesco.ru:/mnt/ada2/usr/home/besco/111/var-log.gz ./
-  scp besco@yabesco.ru:/mnt/ada2/usr/home/besco/111/var-tmp.gz ./
 
 };
 
@@ -131,7 +145,7 @@ importFs() {
   gunzip -c -d /tmp/zroot/tmp/var-log.gz | zfs receive zroot/var/log
   gunzip -c -d /tmp/zroot/tmp/var-tmp.gz | zfs receive zroot/var/tmp
   zfs set mountpoint=/tmp/zroot zroot
-  zfs mount zroot/root 
+  zfs mount zroot/root
 }
 
 modConfig() {
@@ -143,7 +157,7 @@ modConfig() {
   echo "ifconfig_$iFace=\"inet $ipaddr netmask $netmask\"" >>/tmp/rc.conf
   echo "" >>/tmp/rc.conf
   cp /tmp/zroot/root/etc/rc.conf /tmp/zroot/root/etc/rc.conf-bk
-  cp /tmp/rc.conf /tmp/zroot/root/etc/rc.conf 
+  cp /tmp/rc.conf /tmp/zroot/root/etc/rc.conf
 
 }
 
@@ -152,6 +166,8 @@ finish() {
   zfs set mountpoint=/ zroot
   zfs set mountpoint=/ zroot/root
 }
+
+start
 
 setIp
 getDisk
